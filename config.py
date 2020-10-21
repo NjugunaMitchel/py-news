32ee191030912d7056412ecb2aac13f1df649be4
mishel254 <sonimichie@gmail.com>
2020-10-21 16:15:55 +0300

    url correction

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q=all&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SOURCE_API_URL = 'https://newsapi.org/v2/top-headlines?category={}&apiKey={}'
 
